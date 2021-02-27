9720e824200214a6654c0021aa78798b2d3682a6
syedecryptr <abc@def.com>
2021-02-27 12:03:26 +0530

    Changes in example file to accodmodate password protected file and in general made it simple to understand.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,43 +19,72 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-  String asset_file_path = 'assets/sample5.rar';
+  String rar_path = 'assets/sample.rar';
+  String rar5_path = 'assets/sample5.rar';
+  String rar_password_path = "assets/rar4-password-junrar.rar";
   String file_path;
   String destination_path ;
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
+  Directory tempDir;
+
+  extraction() async{
+    await extract_file(rar_path);
+    await extract_file(rar5_path);
+    await extract_file(rar_password_path, password:"junrar");
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
+  // prints content of the temporary directory.
+  print_files() async {
+    print("\n");
+    print("extraction done ... input and output files : ");
+    tempDir.list(recursive: true, followLinks: false)
+        .listen((FileSystemEntity entity) {
+      print(entity.path);
+    });
+  }
 
+  delete_file() async{
+    tempDir.list(recursive: true, followLinks: false)
+        .listen((FileSystemEntity entity) {
+      new File(entity.path).deleteSync();
+    });
+  }
 
-    print("inside init platform state");
-
-    Directory tempDir = await getTemporaryDirectory();
-    file_path = join(tempDir.path, basename(asset_file_path));
+  Future<List<String>>get_input_and_destination_path(asset_file_path) async{
+    // for this example
+    // inside tempDir rar files kept and later
+    // extracted outputs are also stored in the same directory.
+    tempDir = await getTemporaryDirectory();
+    //empty the directory removing previous results.
+    await delete_file();
+    var input_path = join(tempDir.path, basename(asset_file_path));
     // inside the file_path first copy the file from the assets folder.
-    // as there I dont know direct way to find the asset folder.
 
     ByteData data = await rootBundle.load(asset_file_path);
     List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-    await File(file_path).writeAsBytes(bytes);
+    await File(input_path).writeAsBytes(bytes);
     destination_path = tempDir.path;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+    return [input_path, destination_path];
+  }
+
+  Future<void> extract_file(file_path, {password=""}) async {
+    var input_output = await get_input_and_destination_path(file_path);
+    var input_file_path = input_output[0];
+    var destination_path = input_output[1];
+    // Extraction may fail, so we use a try/catch PlatformException.
     try {
-      await UnrarFile.extract_rar(file_path,  destination_path);
-      tempDir.list(recursive: true, followLinks: false)
-          .listen((FileSystemEntity entity) {
-        print(entity.path);
-      });
-      print("extraction done.");
+      await UnrarFile.extract_rar(input_file_path,  destination_path, password: password);
+      await print_files();
     } catch(e) {
-      print("extraction failed");
+
+      print("extraction failed $e");
     }
+  }
 
-
+  // this is where it all started.
+  @override
+  void initState() {
+    super.initState();
+    extraction();
   }
 
   @override
